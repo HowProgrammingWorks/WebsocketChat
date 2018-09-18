15c38f323aac77431d350d12c9ba9793ce481662
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-19 00:28:55 +0300

    Remove round brackets in single-argument lambdas

--- server.js
+++ server.js
@@ -22,16 +22,16 @@ const ws = new Websocket({
 
 const clients = [];
 
-ws.on('request', (req) => {
+ws.on('request', req => {
   const connection = req.accept('', req.origin);
   clients.push(connection);
   console.log('Connected ' + connection.remoteAddress);
-  connection.on('message', (message) => {
+  connection.on('message', message => {
     const dataName = message.type + 'Data';
     const data = message[dataName];
     console.dir(message);
     console.log('Received: ' + data);
-    clients.forEach((client) => {
+    clients.forEach(client => {
       if (connection !== client) {
         client.send(data);
       }
